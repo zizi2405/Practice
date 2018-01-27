26a38e50fc9075c533a83ce1282bfe7bcd675b0b
zizi <zizi2405@yandex.ru>
2018-01-27 10:41:49 +0300

    game_V1 done

--- gamej.js
+++ gamej.js
@@ -4,33 +4,33 @@ var Choice = prompt('сделайте свой выбор', '');
 
     if (Choice=="камень") {
         var a = Math.round(Math.random() * 10);
-            if (a < 4) {
+            if (a <= 3) {
             alert ('Выбор компьютера Камень и НИЧЬЯ');
-        }   else if (a > 4 && a < 7) {
+        }   else if (a > 3 && a < 7) {
             alert ('Выбор компьютера Ножницы и вы выиграли');
-        }   else if (a > 7) {
+        }   else if (a >= 7) {
             alert ('Выбор компьютера бумага и вы проиграли');
         }
     }
 
     else if (Choice=="ножницы") {
         var a = Math.round(Math.random() * 10);
-            if (a < 4) {
+            if (a <= 3) {
             alert ('Выбор компьютера Камень и вы проиграли');
-        }   else if (a > 4 && a < 7) {
+        }   else if (a > 3 && a < 7) {
             alert ('Выбор компьютера Ножницы и НИЧЬЯ');
-        }   else if (a > 7) {
+        }   else if (a >= 7) {
             alert ('Выбор компьютера бумага и вы выиграли');
         }
     }
 
     else if (Choice=="бумага") {
         var a = Math.round(Math.random() * 10);
-            if (a < 4) {
+            if (a <= 3) {
             alert ('Выбор компьютера Камень и вы выиграли');
-        }   else if (a > 4 && a < 7) {
+        }   else if (a > 3 && a < 7) {
             alert ('Выбор компьютера Ножницы и проиграли');
-        }   else if (a > 7) {
+        }   else if (a >= 7) {
             alert ('Выбор компьютера бумага и НИЧЬЯ');
         }
     }
